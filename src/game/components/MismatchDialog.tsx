3d662d59fc492b2fb561e59d59bd0a4c57564af9
Aaron William Po <apo2@uwo.ca>
2024-01-11 22:49:53 -0500

    add useInView

--- src/game/components/MismatchDialog.tsx
+++ src/game/components/MismatchDialog.tsx
@@ -4,8 +4,17 @@ const MismatchDialog: FC<{
   funFact: string;
   mismatchRef: RefObject<HTMLDialogElement>;
 }> = ({ funFact, mismatchRef }) => (
-  <dialog className="modal modal-bottom bg-transparent shadow-lg" ref={mismatchRef}>
-    <div className="modal-box space-y-3 bg-primary text-center">
+  <dialog
+    className="modal modal-bottom bg-transparent shadow-lg"
+    ref={mismatchRef}
+    onClick={() => {
+      mismatchRef.current?.close();
+    }}
+  >
+    <div
+      className="modal-box space-y-3 bg-primary text-center"
+      onClick={(e) => e.stopPropagation()}
+    >
       <h1 className="text-2xl font-bold">Mismatch!</h1>
       <h2 className="text-md">Fun fact about me: {funFact}</h2>
     </div>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,6 +4,7 @@ import aboutInfo from '@/content/pages/about.json';
 import Image from 'next/image';
 import Head from 'next/head';
 import { FC } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 export interface AboutPageProps {
   title: string;
@@ -55,7 +56,85 @@ const TechCard: FC<{
   );
 };
 
+const TechSection: FC<{
+  item: Required<Content>;
+}> = ({ item }) => {
+  const [frontEndRef, frontEndInView] = useInView();
+  const [backEndRef, backEndInView] = useInView();
+  const [databasesRef, databasesInView] = useInView();
+  const [otherRef, otherInView] = useInView();
+  const [currentlyLearningRef, currentlyLearningInView] = useInView();
+
+  return (
+    <div className="mt-8">
+      {item.tech.map((tech, index) => {
+        const refs = [
+          { ref: frontEndRef, inView: frontEndInView },
+          { ref: backEndRef, inView: backEndInView },
+          { ref: databasesRef, inView: databasesInView },
+          { ref: otherRef, inView: otherInView },
+          { ref: currentlyLearningRef, inView: currentlyLearningInView },
+        ];
+
+        return (
+          <div
+            key={tech.category}
+            ref={refs[index].ref}
+            className={`${
+              refs[index].inView ? 'animate-fade animate-once animate-ease-in' : ''
+            }`}
+          >
+            <h3 className="my-3 text-2xl font-bold uppercase">{tech.category}</h3>
+            <div className="grid grid-cols-2 gap-2 md:grid-cols-6">
+              {tech.stack.map((stack) => (
+                <TechCard stack={stack} key={stack.link} />
+              ))}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+interface IAboutSectionProps {
+  item: Content;
+  refValue: (node?: Element | null | undefined) => void;
+  inView: boolean;
+}
+
+const AboutSection: FC<IAboutSectionProps> = ({ item, refValue, inView }) => {
+  return (
+    <section
+      key={item.id}
+      className={`my-3 w-full text-xl ${
+        inView ? 'animate-fade animate-once animate-ease-in' : ''
+      }`}
+      ref={refValue}
+    >
+      <div>
+        <h2 className="my-4 text-3xl font-extrabold md:text-5xl">{item.heading}</h2>
+
+        <div className="space-y-4">
+          {item.text.split('\n').map((line, index) => (
+            <p key={index}>{line}</p>
+          ))}
+        </div>
+
+        {item.tech && <TechSection item={item as Required<Content>} />}
+      </div>
+    </section>
+  );
+};
+
 const AboutPage: NextPage<AboutPageProps> = ({ content }) => {
+  const [preambleRef, preambleInView] = useInView();
+  const [goalsRef, goalsInView] = useInView();
+  const [technologiesRef, technologiesInView] = useInView();
+  const [projectsRef, projectsInView] = useInView();
+  const [interestsRef, interestsInView] = useInView();
+  const [contactRef, contactInView] = useInView();
+
   return (
     <>
       <Head>
@@ -63,48 +142,42 @@ const AboutPage: NextPage<AboutPageProps> = ({ content }) => {
         <meta name="description" content={aboutInfo.preamble} />
       </Head>
       <article className="my-20 flex min-h-dvh flex-col items-center md:my-16">
-        <header className="w-10/12 lg:w-7/12">
-          <h1 className="my-7 text-5xl font-extrabold md:my-10 md:text-8xl">About Me</h1>
-          <div className="space-y-4">
-            {aboutInfo.preamble.split('\n').map((line, index) => (
-              <p className="text-xl" key={index}>
-                {line}
-              </p>
-            ))}
-          </div>
-        </header>
-        {content.map((item) => (
-          <section key={item.id} className="my-3 w-10/12 text-xl lg:w-7/12">
-            <div>
-              <h2 className="my-4 text-3xl font-extrabold md:text-5xl">{item.heading}</h2>
-
-              <div className="space-y-4">
-                {item.text.split('\n').map((line, index) => (
-                  <p key={index}>{line}</p>
-                ))}
-              </div>
-
-              {item.tech && (
-                <div className="mt-8">
-                  {item.tech.map((tech) => {
-                    return (
-                      <div key={tech.category}>
-                        <h3 className="my-3 text-2xl font-bold uppercase">
-                          {tech.category}
-                        </h3>
-                        <div className="grid grid-cols-2 gap-2 md:grid-cols-6">
-                          {tech.stack.map((stack) => (
-                            <TechCard stack={stack} key={stack.link} />
-                          ))}
-                        </div>
-                      </div>
-                    );
-                  })}
-                </div>
-              )}
+        <div className="w-10/12 space-y-8 lg:w-6/12">
+          <header className="w-full">
+            <h1 className="my-7 text-5xl font-extrabold md:my-10 md:text-8xl">
+              About Me
+            </h1>
+            <div
+              className={`space-y-4 ${
+                preambleInView ? 'animate-fade animate-once animate-ease-in' : ''
+              }`}
+              ref={preambleRef}
+            >
+              {aboutInfo.preamble.split('\n').map((line, index) => (
+                <p className="text-xl" key={index}>
+                  {line}
+                </p>
+              ))}
             </div>
-          </section>
-        ))}
+          </header>
+          {content.map((item, index) => {
+            const refs = [
+              { ref: goalsRef, inView: goalsInView },
+              { ref: technologiesRef, inView: technologiesInView },
+              { ref: projectsRef, inView: projectsInView },
+              { ref: interestsRef, inView: interestsInView },
+              { ref: contactRef, inView: contactInView },
+            ];
+            return (
+              <AboutSection
+                item={item}
+                key={item.id}
+                refValue={refs[index].ref}
+                inView={refs[index].inView}
+              />
+            );
+          })}
+        </div>
       </article>
     </>
   );
