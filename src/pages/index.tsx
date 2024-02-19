42b96d41241ba27ecdf39e210e574cf7a7635031
Aaron William Po <apo2@uwo.ca>
2024-02-18 23:05:30 -0500

    Add metadata tags for SEO and social media sharing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,36 @@ import Socials from '@/components/homepage/Socials';
 import Image from 'next/image';
 import Head from 'next/head';
 
+const metadata = {
+  title: process.env.NEXT_PUBLIC_SITE_NAME,
+  description: "Aaron Po's personal portfolio.",
+  keywords: [
+    'Aaron Po',
+    'personal portfolio',
+    'developers london ontario',
+    'web developers london ontario',
+    'web developers',
+    'web developers london',
+    'web developers ontario',
+    'web developers canada',
+  ],
+};
+
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>{process.env.NEXT_PUBLIC_SITE_NAME}</title>
         <meta name="description" content="Aaron Po's personal website" />
+        <meta name="keywords" content={metadata.keywords.join(', ')} />
+        <meta name="author" content="Aaron Po" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={metadata.title} />
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_SITE_URL} />
+        <meta property="og:image" content="/background.jpg" />
+        <meta property="og:image:alt" content={metadata.title} />
       </Head>
       <div className="relative flex h-dvh w-full flex-col items-center justify-center">
         <Image
