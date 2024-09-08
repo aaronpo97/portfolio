8c9e881c760e6e67c4dcfc13be3127d1b45ed706
Aaron William Po <apo2@uwo.ca>
2024-09-08 01:33:15 -0400

    Update leaderboard entry ui

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -1,21 +1,9 @@
 import { FC } from 'react';
 
-interface SpinnerProps {
-  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-}
-
-const Spinner: FC<SpinnerProps> = ({ size = 'md' }) => {
-  const spinnerWidths: Record<NonNullable<SpinnerProps['size']>, `w-[${number}px]`> = {
-    xs: 'w-[45px]',
-    sm: 'w-[90px]',
-    md: 'w-[135px]',
-    lg: 'w-[180px]',
-    xl: 'w-[225px]',
-  };
-
+const Spinner: FC = () => {
   return (
     <div role="alert" className="text-center">
-      <span className={`loading loading-spinner loading-${spinnerWidths[size]}`} />
+      <span className={`loading loading-spinner loading-lg`} />
     </div>
   );
 };
